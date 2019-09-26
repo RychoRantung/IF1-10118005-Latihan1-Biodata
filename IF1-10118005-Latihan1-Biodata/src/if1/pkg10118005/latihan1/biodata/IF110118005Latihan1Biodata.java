c3564f9012afa9eec71b59173ffcd6947e9686b3
Rycho Rantung <55700409+RychoRantung@users.noreply.github.com>
2019-09-26 13:16:32 +0700

    Add files via upload

---
+++ IF1-10118005-Latihan1-Biodata/src/if1/pkg10118005/latihan1/biodata/IF110118005Latihan1Biodata.java
@@ -0,0 +1,39 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package if1.pkg10118005.latihan1.biodata;
+
+/**
+ *
+ * @author USER
+ * NAMA   : Rycho Rantung
+ * KELAS  : IF-1
+ * NIM    : 10118005
+ * Deskripsi Program : Program ini berisi program untuk menampilkan biodata
+ 
+ 
+ 
+ 
+ */
+public class IF110118005Latihan1Biodata {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        System.out.println("BIODATA ROCKSTAR" + "\n" + "------------------");
+        System.out.println("NAMA\t\t: Rycho Rantung");
+        System.out.println("ALAMAT\t\t: Gang Jati Dago III");
+        System.out.println("Tempat Lahir\t: Palu");
+        System.out.println("Tanggal Lahir\t: 30 Maret 2000");
+        System.out.println("------------------");
+        System.out.println("HOBI\t\t: Nonton");
+        System.out.println("MAKES\t\t: Mie");
+        System.out.println("MIKES\t\t: Air");
+    }
+    
+}
